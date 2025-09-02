e051029dcb884ad63ba3940767b91a16f11fb57f
Jon <me@jon-keatley.com>
2025-09-02 17:25:31 +0100

    added shader loading

--- webgl/src/main.rs
+++ webgl/src/main.rs
@@ -4,10 +4,65 @@ use web_sys::{window, HtmlCanvasElement};
 use web_sys::WebGl2RenderingContext;
 use web_sys::{WebGlProgram,WebGlShader};
 
-/*fn load_shader() -> WebGLProgram
+const BASIC_VERTEX_SHADER_SRC:&str = "
+attribute vec4 aVertexPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+";
+
+const BASIC_FRAGMENT_SHADER_SRC:&str = "
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+";
+
+fn load_shader(context:&WebGl2RenderingContext, shader_type:u32, source: &str) 
+-> Result<WebGlShader, String>
 {
-    let vertex_shader = 
-}*/
+    let has_shader = context.create_shader(shader_type);
+    
+    if let Some(shader) = has_shader
+    {
+        context.shader_source(&shader, source);
+        context.compile_shader(&shader);
+        if context.get_shader_parameter(
+            &shader,
+            WebGl2RenderingContext::COMPILE_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            return Ok(shader)
+        }
+    }
+    Err("Unable to compile shader".to_string()) //get_shader_info_log
+}
+
+fn create_program(context:&WebGl2RenderingContext, vertex_src: &str, fragment_src: &str) 
+-> Result<WebGlProgram, String>
+{
+    let vertex_shader = load_shader(
+                                                    &context,
+                                                    WebGl2RenderingContext::VERTEX_SHADER,
+                                                    vertex_src
+    )?;
+    let fragment_shader = load_shader(
+                                                        &context,
+                                                        WebGl2RenderingContext::FRAGMENT_SHADER,
+                                                        fragment_src
+                                                        )?;
+    let has_program = context.create_program();
+    if let Some(program) = has_program
+    {
+        context.attach_shader(&program, &vertex_shader);
+        context.attach_shader(&program, &fragment_shader);
+    }
+    
+    Err("WIP".to_string())
+    
+}
 
 fn start_app() {
     let document = window()
@@ -28,9 +83,15 @@ fn start_app() {
         context.clear_color(1.0, 0.0, 1.0, 1.0);
         context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
         
-       // context.create_shader(WebGlRenderingContext::VERTEX_SHADER);
+                
+        let try_program 
+        = create_program(&context, BASIC_VERTEX_SHADER_SRC, BASIC_FRAGMENT_SHADER_SRC);
         
-        //context.attach_shader(program, shader);
+        if let Ok(program) = try_program
+        {
+            context.use_program(Some(&program));
+            
+        }
     }
 
 
